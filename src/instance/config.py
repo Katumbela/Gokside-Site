dd7311917fa6bb1b8de0057e44e3ca6823f8c7fc
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-09-21 17:31:00 +0100

    ms

---
+++ src/instance/config.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+# Obtém a data atual
+data_atual = datetime.now()
+
+# Formata a data no formato '10-Apr-2023'
+data_formatada = data_atual.strftime('%d-%b-%Y')
+
+
+email = 'katumbela.meucarrinho@gmail.com'
+password = 'jgluewhazvnkddjp'
+label = 'Inbox'
+from_date = data_formatada
+to_date = '1-May-2024'
