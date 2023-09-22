bd073b95255897a41d2fc590536c07787d978b27
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-09-22 12:43:00 +0100

    Atualizar o main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+se# -*- coding:utf-8 -*-
 ###
 # --------------------------------------------------------------
 #
@@ -221,6 +221,6 @@ def register():
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+        #db.create_all()
     app.run(debug=True)
