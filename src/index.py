4cd9c9d5a005c92cd2398143da840b549f3da1d2
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-10-10 10:16:30 +0100

    Update index.py

--- src/index.py
+++ src/index.py
@@ -220,6 +220,6 @@ def register():
 
 
 if __name__ == '__main__':
-    #with app.app_context():
-        #db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
